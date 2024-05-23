fe04c685138fbef98300e788f5ee75f49c9cc9a0
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-23 23:54:36 +0530

    adding styles using dom manipulation

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 // DOM Manipulation
-// Selecting Elements
 
-// get element by id
-const title = document.getElementById("title");
+const title = document.querySelector("#main-heading");
 console.log(title);
 
-// get element by class
-const listItems = document.getElementsByClassName("list-items");
+title.style.color = "red"; // turns title red
+
+const listItems = document.querySelectorAll(".list-items");
 console.log(listItems);
 
-// get elements by tag tag name
-const list = document.getElementsByTagName("ul");
-console.log(list);
-
-// query selector
-const first = document.querySelector("li");
-console.log(first);
-
-// query selector for all items
-const all = document.querySelectorAll("li");
-console.log(all);
+listItems.forEach((item) => {
+  item.style.fontSize = "1.5rem";
+});
